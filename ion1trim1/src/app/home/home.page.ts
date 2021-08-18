8c3d766ef8c8ac09e39a62a4813705ffa2cd5b3c
Gabr17Rodri <eu.gabriel.araujo.rodrigues@gmail.com>
2021-08-18 20:48:43 -0300

    18/08

--- ion1trim1/src/app/home/home.page.ts
+++ ion1trim1/src/app/home/home.page.ts
@@ -6,7 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  login = '';
+  senha = '';
+  mensagem = '';
+
 
   constructor() {}
 
+  validar(): void{
+    if(this.login === 'admin' && this.senha === '1234'){
+      this.mensagem = 'Usuário logado!';
+    }
+    else{
+      this.mensagem = 'Falha de autenticação';
+    }
+  }
 }
